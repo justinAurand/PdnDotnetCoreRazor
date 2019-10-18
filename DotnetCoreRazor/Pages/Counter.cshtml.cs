58a4df7297137cf163a43cc132087d44c48c17e3
Justin Aurand <justin.aurand@energage.com>
2019-10-18 10:48:13 -0400

    add working counter page

---
+++ DotnetCoreRazor/Pages/Counter.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace DotnetCoreRazor.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class CounterModel : PageModel
+    {
+        public int Number { get; set; }
+
+        public void OnPost()
+        {
+            int number = int.Parse(Request.Form["number"]);
+            Number = number + 1;
+        }
+    }
+}
