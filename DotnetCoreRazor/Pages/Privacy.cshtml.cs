6776f7a38a8a70990b6e13010da8269390cfaa6d
Justin Aurand <justin.aurand@energage.com>
2019-10-18 10:57:48 -0400

    add another counter as razor page view

--- DotnetCoreRazor/Pages/Privacy.cshtml.cs
+++ DotnetCoreRazor/Pages/Privacy.cshtml.cs
@@ -1,19 +1,9 @@
 ﻿namespace DotnetCoreRazor.Pages
 {
     using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.Extensions.Logging;
 
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+        public void OnGet() { }
     }
 }
